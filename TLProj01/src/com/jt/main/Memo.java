8decc28d1b11f4ba178b24f22834d0d8899f6bb0
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-09-03 13:55:52 +0530

    Added one more

---
+++ TLProj01/src/com/jt/main/Memo.java
@@ -0,0 +1,15 @@
+package com.jt.main;
+
+public class Memo {
+	
+	
+	public String chat() {
+		return "Welcome to git";
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
