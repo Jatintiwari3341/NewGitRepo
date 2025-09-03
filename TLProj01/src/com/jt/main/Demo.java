60d7f0cab96e4b8414613106c6796140a3ea7c74
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-09-03 12:06:12 +0530

    fisrt commit

---
+++ TLProj01/src/com/jt/main/Demo.java
@@ -0,0 +1,14 @@
+package com.jt.main;
+
+public class Demo {
+
+	
+	public String message() {
+		return "Hello Guys";
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
