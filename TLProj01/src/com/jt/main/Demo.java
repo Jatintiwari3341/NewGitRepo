b7e2458cdb31a0da60172ae906d916a97c18b1bd
Jatintiwari3341 <tripathijatin319@gmail.com>
2025-09-03 14:53:13 +0530

    Add new

--- TLProj01/src/com/jt/main/Demo.java
+++ TLProj01/src/com/jt/main/Demo.java
@@ -6,6 +6,9 @@ public class Demo {
 	public String message() {
 		return "Hello  my Guys ";
 	}
+	public String byMessage() {
+		return "by dosto";
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
